02321c6b1de5bd477e4c07159e0a2db02687d4bc
Pierce Scott <p.scott1396@gmail.com>
2025-06-07 21:26:28 -0600

    added card assets

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,7 +8,6 @@ public class LevelManager : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // You can check if the "other" collider belongs to the player (or something else)
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player entered the trigger!");
